2c9c4c3f8dcbdace966c4712d7f35ff0bd699fe6
lauradacol <laura.dacol@gmail.com>
2017-10-30 20:45:12 -0200

    falta implementar split

--- BTREE/btree.cpp
+++ BTREE/btree.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 //Grau da árvore
-const int T = 3;
+const int T = 2;
+
+//Posição do split
+const int S = ((2*T)/2)-1;
 
 struct Node {
 	long my_pos;
@@ -39,6 +42,59 @@ struct Node {
 		size++;
 	}
 	
+	//Encontra o nodo folha para a chave
+	Node * findNode(int key){
+		if(isLeaf()){
+			return this;
+		}
+		else{
+			int i;
+			//Encontra a posição no vetor de filhos de parentNode 			
+			for(i=0; i<size && this->values[i] < key; i++);
+			return(this->sons[i]->findNode(key));
+		}
+	}
+	
+	//Insere a chave no nodo certo
+	Node * insert(int key){
+		this->findNode(key)->insertLeaf(key);
+	}
+	
+	//Dar split no nodo, se ele tá cheio
+	Node * split(){
+		//Se está cheio, splitar
+		if(this->isFull()){
+			Node parent = new Node;
+			Node rightSon = new Node;
+			
+			parent->values[0] = this.values[S];
+			
+			//Copia os valores para o rightSon
+			j = 0;
+			for(int i=S+1; i<size; i++){
+				rightSon->values[j] = this.values[i];
+				j++;
+			}
+			
+			//Apaga os valores do nodo original
+			for(int i=S; i<size; i++){
+				this.values[i] = 0;
+			}
+			size = S;
+			
+			//Arruma os filhos
+			parent->sons[0] = this;
+			parent->sons[1] = rightSon;
+			
+			return parent;			
+		}
+		
+		//Se o nodo não está cheio
+		else{
+			return this;
+		}
+	}
+	
 	//Imprime o nodo
 	void printNodo(){
 		printf("Nodo: ");
@@ -58,54 +114,30 @@ struct Node {
 		}
 		else{
 			printf("Sem filhos\n");
-		}		
-		
-	}	
-  
+		}				
+	}	  
 };
 
 struct Tree{
 	Node * root;
 	long position;
 	
+	//Construtor		
+	Tree(){
+		root = new Node;
+	}		
+			
 	//Retorna true se a árvore está vazia
 	bool isEmpty() {return root == NULL;}
-	
-	//Insere na raiz
-	void insertRoot(int key){		
-		this->root = new Node();
-		root->insertLeaf(key);
-		//return this->root;
-	}
-	
-	//Achar o nodo folha correto para a chave
-	Node * findNode(Node * parentNode, int key){
 
-		//Enquanto o nodo não for folha
-		while(!(parentNode->isLeaf())){
-			int i;
-			//Encontra a posição no vetor de filhos de parentNode 			
-			for(i=0; (i<parentNode->size && key>parentNode->values[i]); i++);
-			
-			//Chama recursivamente a função para o nodo filho
-			findNode(parentNode->sons[i], key);						
-		}
-		
-		parentNode->insertLeaf(key);
-		//parentNode.split();		
-		return parentNode;			
-	}
+	//Achar o nodo folha correto para a chave
+	Node * findNode(int key){
+		return this->root->findNode(key);
+	}	
 	
-	//Função de inserir
-	void * insert(int key){
-		if(this->isEmpty()){
-			insertRoot(key);
-		}
-		else{
-			findNode(root,key)->insertLeaf(key);
-		}
-		
-		//this->isEmpty() ? insertRoot(key) : findNode(root, key);
+	//Função de inserir	
+	Node * insert(int key){
+		return this->root->insert(key);
 	}
 };
 
@@ -114,6 +146,13 @@ int main(){
 	Tree * t = new Tree();
 		
 	t->insert(6);
+	t->insert(4);
+	t->insert(8);
+	t->insert(10);
+	
+	//cout << (t->root->isFull() ? "Cheia\n" : "Não cheia\n");
+	
+	/*
 	
 	Node * n1 = new Node();
 	n1->insertLeaf(4);
@@ -123,10 +162,9 @@ int main(){
 	n2->insertLeaf(8);
 	n2->insertLeaf(20);
 	t->root->sons[1] = n2;
+		*/
+				
 	
-	//cout << (t->root->isLeaf() ? "É folha" : "Não é folha");
-	
-	t->insert(10);			
 	t->root->printNodo();
 	t->root->printFilho();	
 				
